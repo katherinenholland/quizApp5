c2b082ff382c08641c0ad2ec026e2da1814d770f
katherinenholland <katherinenholland@gmail.com>
2014-06-19 18:05:58 -0400

    index.html and main.css
    updates

--- app.js
+++ app.js
@@ -1,119 +1,71 @@
 $(document).ready(function() {
-	instructions();
-	question1();
-	question2();
-	question3();
-	question4();
-	question5();
-	score();
-	newGame();
-}); //end of document ready function
-var finalScore = [];//array that holds score for each question answer
+	var finalScore = [];//array that holds score for each question answer
+	var totalScore = [];//array used to find sum of score
 
-answer1 = function() {
-	$("input:radio[name=b]").click(function() {
-		var a = parseInt( $(this).val() );
-		finalScore.push(a);
-	})
-};//pushes score of Question 1 to finalScore []
-
-answer2 = function() {
-	$("input:radio[name=g]").click(function() {
-		var a = parseInt( $(this).val() );
-		finalScore.push(a);
-	})
-};//pushes score of Question 2 to finalScore []
-
-answer3 = function() {
-	$("input:radio[name=b]").click(function() {
-		var a = parseInt( $(this).val() );
-		finalScore.push(a);
-	})
-};//pushes score of Question 3 to finalScore[]
-
-answer4 = function() {
-	$("input:radio[name=s]").click(function() {
-		var a = parseInt( $(this).val() );
-		finalScore.push(a);
-	})
-};//pushes score of Question 4 to finalScore[]
-
-answer5 = function() {
-	$("input:radio[name=c]").click(function() {
-		var a = parseInt( $(this).val() );
-		finalScore.push(a);
-	})
-};//pushes score of Question 5 to finalScore[]
-
-instructions = function() {
-	$(".button1").click(function() {
-	$(".question1").show();
+	$(".instructionButton").click(function() {
+		$(".question1").show();
 		$(".instructions").hide();	
-	})
-};
+		});
 
-question1 = function() {
-	$(".button2").click(function() {
-		answer1();
-	$(".question2").show();
-	$(".question1").hide();		
-	})
+	$(".question1Button").click(function() {
+		if ($(".correctAnswer").is(":checked")) {
+			finalScore.push(1);
+		}
+		$(".question2").show();
+		$(".question1").hide();		
+	});
 
-};//records user selection for Question 1
-
-question2 = function() {
-	$(".button3").click(function() {
-		answer2();
+	$(".question2Button").click(function() {
+		if ($(".correctAnswer").is(":checked")) {
+			finalScore.push(1);
+		}
 		$(".question3").show();
 		$(".question2").hide();	
-	})
-};//records user selection for Question 2
+	});
 
-question3 = function() {
-	$(".button4").click(function() {
-		answer3();
+	$(".question3Button").click(function() {
+		if ($(".correctAnswer").is(":checked")) {
+			finalScore.push(1);
+		}
 		$(".question4").show();
 		$(".question3").hide();	
-	})
-};//records user selection for Question 3
+	});
 
-question4 = function() {
-	$(".button5").click(function() {
-		answer4();
+	$(".question4Button").click(function() {
+		if ($(".correctAnswer").is(":checked")) {
+			finalScore.push(1);
+		}
 		$(".question5").show();
 		$(".question4").hide();	
-	})
-};//records user selection for Question 4
+	});
+	
 
-question5 = function() {
-	$(".button6").click(function() {
-		answer5();
+	$(".question5Button").click(function() {
+		if ($(".correctAnswer").is(":checked")) {
+			finalScore.push(1);
+		} 
+		$(".score").show();
+		$(".question5").hide();	
+	});	
+
+	total();
+	
+	$(".score").text(totalScore + " out of 5 Correct!");
 		
-	})
-};//records user selection for Question 5
-var totalScore = [];
+	$('.newGameButton').click(function() {
+		location.reload();
+	});
+
+}); //end of document ready function
+
+
 
 total = function() {
 	var sum = 0;
-	for (i = 0; i< finalScore.length; i++)
+	for (i = 0; i < 5; i++)
 	{
 	sum = sum + finalScore[i];	
 	}
 	totalScore.push(sum);
 };//totals the finalScore[] and pushes the total to totalScore
-
-score = function() {
-	$(".button6").click(function() {
-		total();
-		$(".score").text(totalScore);
-		$(".score").show();
-		$(".question5").hide();
-	})
-}; //changes finalScore[] strings to variables; finds total score of quiz; displays final score 
-
-newGame = function() {
-	$('.button7').click(function() {
-		location.reload();
-	})
-}; //reloads page to start a new game
 
